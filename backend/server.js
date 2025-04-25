fd61b0891760f04a1734cd6be078824a6e6c360e
qnchoyy <qni191488@abv.bg>
2025-04-25 16:09:48 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import { PORT } from './config/env.js'
+import connectToDB from './config/db.js';
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get('/api/test', (req, res) => {
     res.send('API is running 🚀');
 });
+
+connectToDB();
 
 app.listen(PORT, () => {
     console.log(`✅ Server is running on http://localhost:${PORT}`);
