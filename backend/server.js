e20d9d796010e49cf0c03928ce3ce553d680b35d
qnchoyy <qni191488@abv.bg>
2025-04-28 19:56:59 +0300

    Add booking routes to main file

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { PORT } from './config/env.js'
 import connectToDB from './config/db.js';
 import serviceRouter from './routes/service.routes.js';
+import bookingRouter from './routes/booking.routes.js';
 
 connectToDB();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/services', serviceRouter);
+app.use('/api/bookings', bookingRouter);
 
 app.listen(PORT, () => {
     console.log(`✅ Server is running on http://localhost:${PORT}`);
