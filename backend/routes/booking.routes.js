b31d55a36a372b5dcf65c252157d4a25a709ecb0
qnchoyy <qni191488@abv.bg>
2025-05-02 02:38:09 +0300

    Update booking.routes.js

--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createBooking, getAllBookings, getMyBookings } from '../controllers/booking.controller.js';
+import { createBooking, getAllBookings, getMyBookings, updateBookingStatus } from '../controllers/booking.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { adminOnly } from '../middlewares/admin.middleware.js';
 
@@ -10,5 +10,7 @@ bookingRouter.post('/create-booking', protect, createBooking);
 bookingRouter.get('/all-bookings', protect, adminOnly, getAllBookings);
 
 bookingRouter.get('/my-bookings', protect, getMyBookings);
+
+bookingRouter.patch('/:id/status', protect, adminOnly, updateBookingStatus);
 
 export default bookingRouter;
