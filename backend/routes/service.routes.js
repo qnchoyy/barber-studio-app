c1de7286c2d8ad735639fa36125f0f60f6645495
qnchoyy <qni191488@abv.bg>
2025-05-01 19:13:57 +0300

    Update service.routes.js

--- backend/routes/service.routes.js
+++ backend/routes/service.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { createService, getAllServices } from '../controllers/service.controller.js';
+import { adminOnly } from '../middlewares/admin.middleware.js';
+import { protect } from '../middlewares/auth.middleware.js';
 
 const serviceRouter = Router();
 
 serviceRouter.get('/all-services', getAllServices);
 
-serviceRouter.post('/create-service', createService);
+serviceRouter.post('/create-service', protect, adminOnly, createService);
 
 export default serviceRouter;
