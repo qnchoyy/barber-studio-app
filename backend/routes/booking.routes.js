c0b813aa620d145ccb5570ab488a06dbcbdac1ab
qnchoyy <qni191488@abv.bg>
2025-05-01 19:56:35 +0300

    Update booking.routes.js

--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createBooking, getAllBookings } from '../controllers/booking.controller.js';
+import { createBooking, getAllBookings, getMyBookings } from '../controllers/booking.controller.js';
+import { protect } from '../middlewares/auth.middleware.js';
 
 const bookingRouter = Router();
 
-bookingRouter.post('/create-booking', createBooking);
+bookingRouter.post('/create-booking', protect, createBooking);
 
 bookingRouter.get('/all-bookings', getAllBookings);
+
+bookingRouter.get('/my-bookings', protect, getMyBookings);
 
 export default bookingRouter;
