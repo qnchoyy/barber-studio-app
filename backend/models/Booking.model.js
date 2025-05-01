73db61bf34442fbf7126eea3ac2ce14e67fda943
qnchoyy <qni191488@abv.bg>
2025-05-01 19:47:26 +0300

    Update Booking.model.js

--- backend/models/Booking.model.js
+++ backend/models/Booking.model.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 const bookingSchema = new mongoose.Schema(
     {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
         userName: {
             type: String,
             required: [true, 'User name is required'],
