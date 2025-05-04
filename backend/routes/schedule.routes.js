23c7fcbc165575f221aecea62df466d5dfc9c5f8
qnchoyy <qni191488@abv.bg>
2025-05-04 19:46:00 +0300

    Update schedule.routes.js

--- backend/routes/schedule.routes.js
+++ backend/routes/schedule.routes.js
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import { protect } from '../middlewares/auth.middleware.js';
 import { adminOnly } from '../middlewares/admin.middleware.js';
-import { createSchedule } from '../controllers/controllers/schedule.controller.js';
+import { createSchedule, deleteScheduleByDay, getAllSchedules, getScheduleByDay, updateSchedule } from '../controllers/schedule.controller.js';
 
 const scheduleRouter = Router();
 
 scheduleRouter.post('/create', protect, adminOnly, createSchedule);
+scheduleRouter.patch('/update/:day', protect, adminOnly, updateSchedule);
+scheduleRouter.get('/day/:day', protect, adminOnly, getScheduleByDay);
+scheduleRouter.get('/all', protect, adminOnly, getAllSchedules);
+scheduleRouter.delete('/delete/:day', protect, adminOnly, deleteScheduleByDay);
 
 export default scheduleRouter;
