3c4bcda49c2a31de3a4f49bbd3a23b772adb5ba7
qnchoyy <qni191488@abv.bg>
2025-05-05 14:53:03 +0300

    Update env.js

--- backend/config/env.js
+++ backend/config/env.js
@@ -7,5 +7,6 @@ export const PORT = process.env.PORT || 5500;
 export const {
     SERVER_URL,
     DB_URI,
-    JWT_SECRET
+    JWT_SECRET,
+    TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER,
 } = process.env;
