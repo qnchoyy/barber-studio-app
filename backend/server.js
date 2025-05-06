230fc7776754f90dfdb6db74042514a17c951ce9
qnchoyy <qni191488@abv.bg>
2025-05-06 19:40:51 +0300

    Add userRouter to main file

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import cors from 'cors';
 
 import { PORT } from './config/env.js'
 import connectToDB from './config/db.js';
+import { autoCompleteBookings } from './jobs/autoCompleteBookings.job.js';
 
 import serviceRouter from './routes/service.routes.js';
 import bookingRouter from './routes/booking.routes.js';
 import authRouter from './routes/auth.routes.js';
-import { autoCompleteBookings } from './jobs/autoCompleteBookings.job.js';
 import scheduleRouter from './routes/schedule.routes.js';
+import userRouter from './routes/user.routes.js';
 
 connectToDB();
 autoCompleteBookings();
@@ -22,6 +23,7 @@ app.use('/api/services', serviceRouter);
 app.use('/api/bookings', bookingRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/schedule', scheduleRouter);
+app.use('/api/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`✅ Server is running on http://localhost:${PORT}`);
