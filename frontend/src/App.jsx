090f929666bd4f542854479f372c4c3852792522
qnchoyy <qni191488@abv.bg>
2025-05-26 17:21:18 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./pages/Home";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <h1 className="text-3xl font-bold text-gray-800">
-          Barber App Frontend 🚀
-        </h1>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> */}
+      </Routes>
     </>
   );
 }
