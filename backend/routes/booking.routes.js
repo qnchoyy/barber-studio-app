88d84111d34a9ae277110bd7f2c3a7c464bac200
qnchoyy <qni191488@abv.bg>
2025-06-05 18:41:34 +0300

    Update booking.routes.js

--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createBooking, deleteBooking, getAllBookings, getAvailableSlots, getBookingById, getMyBookings, updateBookingStatus } from '../controllers/booking.controller.js';
+import { cancelMyBooking, createBooking, deleteBooking, getAllBookings, getAvailableSlots, getBookingById, getMyBookings, updateBookingStatus } from '../controllers/booking.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { adminOnly } from '../middlewares/admin.middleware.js';
 
@@ -18,5 +18,7 @@ bookingRouter.get('/details/:id', protect, adminOnly, getBookingById);
 bookingRouter.delete('/delete/:id', protect, adminOnly, deleteBooking);
 
 bookingRouter.get('/available-slots', protect, getAvailableSlots);
+
+bookingRouter.patch('/cancel/:id', protect, cancelMyBooking);
 
 export default bookingRouter;
