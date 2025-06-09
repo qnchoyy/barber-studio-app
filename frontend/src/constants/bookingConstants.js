88adeeeb0c5c4904dd8ad47683fd0a7b8209ce33
qnchoyy <qni191488@abv.bg>
2025-06-09 18:52:30 +0300

    Create bookingConstants.js

---
+++ frontend/src/constants/bookingConstants.js
@@ -0,0 +1,12 @@
+export const BOOKING_STATUS = {
+    CONFIRMED: 'потвърдена',
+    COMPLETED: 'завършена',
+    CANCELLED: 'отменена',
+};
+
+export const BOOKING_CONSTANTS = {
+    UNKNOWN_TIME_TEXT: 'неизвестно време',
+    UNKNOWN_DATE_TEXT: 'неизвестна дата',
+    DATE_LOCALE: 'bg-BG',
+    CANCELLATION_WINDOW_HOURS: 2,
+};
