0993e4c52b94a07f55b0d036f7c855e2f45672a3
qnchoyy <qni191488@abv.bg>
2025-06-11 11:39:55 +0300

    Add admin statistics routes

--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { cancelMyBooking, createBooking, deleteBooking, getAllBookings, getAvailableSlots, getBookingById, getMyBookings, updateBookingStatus } from '../controllers/booking.controller.js';
+import { cancelMyBooking, createBooking, deleteBooking, getAdminStats, getAllBookings, getAvailableSlots, getBookingById, getMonthlyRevenue, getMyBookings, getPendingBookings, getRecentBookings, updateBookingStatus } from '../controllers/booking.controller.js';
 import { protect } from '../middlewares/auth.middleware.js';
 import { adminOnly } from '../middlewares/admin.middleware.js';
 
@@ -20,5 +20,13 @@ bookingRouter.delete('/delete/:id', protect, adminOnly, deleteBooking);
 bookingRouter.get('/available-slots', protect, getAvailableSlots);
 
 bookingRouter.patch('/cancel/:id', protect, cancelMyBooking);
+
+bookingRouter.get('/admin/stats', protect, adminOnly, getAdminStats);
+
+bookingRouter.get('/admin/recent', protect, adminOnly, getRecentBookings);
+
+bookingRouter.get('/admin/pending', protect, adminOnly, getPendingBookings);
+
+bookingRouter.get('/admin/monthly-revenue', protect, adminOnly, getMonthlyRevenue);
 
 export default bookingRouter;
