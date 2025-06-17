8cbd4b5e0c4d08d129ee13e43b44c8096d04b73f
qnchoyy <qni191488@abv.bg>
2025-06-17 21:57:34 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import AdminLayout from "./components/admin/layout/AdminLayout";
+
+// Public pages
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -8,18 +11,90 @@ import Services from "./pages/Services";
 import BookingConfirmation from "./pages/BookingConfirmation";
 import MyBookings from "./pages/MyBookings";
 
+// Admin pages
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminServices from "./pages/admin/AdminServices";
+
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
-        <Route path="/bookings" element={<MyBookings />} />
+        {/* Public routes with main Navbar */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Navbar />
+              <Login />
+            </>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <>
+              <Navbar />
+              <Register />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <Navbar />
+              <Profile />
+            </>
+          }
+        />
+        <Route
+          path="/services"
+          element={
+            <>
+              <Navbar />
+              <Services />
+            </>
+          }
+        />
+        <Route
+          path="/booking-confirmation"
+          element={
+            <>
+              <Navbar />
+              <BookingConfirmation />
+            </>
+          }
+        />
+        <Route
+          path="/bookings"
+          element={
+            <>
+              <Navbar />
+              <MyBookings />
+            </>
+          }
+        />
+
+        {/* Admin routes with AdminLayout */}
+        <Route
+          path="/admin"
+          element={
+            <AdminLayout>
+              <AdminDashboard />
+            </AdminLayout>
+          }
+        />
+
+        <Route path="/admin/services" element={<AdminServices />} />
       </Routes>
     </>
   );
